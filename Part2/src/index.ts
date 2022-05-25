77b3a241ca23868b349a40954caebf6e78660a7b
mku <80041061+mkuwdev@users.noreply.github.com>
2022-05-25 21:34:28 +0700

    Update index.ts

--- Part2/src/index.ts
+++ Part2/src/index.ts
@@ -239,6 +239,22 @@ const encrypt = async (
 ): Promise<Ciphertext> => {
   const mimc7 = await buildMimc7();
   // [assignment] generate the IV, use Mimc7 to hash the shared key with the IV, then encrypt the plain text
+  const F = mimc7.F;
+
+  const iv = F.toObject(mimc7.multiHash(plaintext, BigInt(0)));
+  const ciphertext : Ciphertext = {
+    iv,
+    data: plaintext.map(
+      (e: bigint, i: number): bigint => {
+        return e + F.toObject(mimc7.hash(
+          sharedKey,
+          iv + BigInt(i)
+        ))
+      }
+    )
+  }
+
+  return ciphertext;
 };
 
 /*
@@ -250,6 +266,19 @@ const decrypt = async (
   sharedKey: EcdhSharedKey,
 ): Promise<Plaintext> => {
   // [assignment] use Mimc7 to hash the shared key with the IV, then descrypt the ciphertext
+  const mimc7 = await buildMimc7();
+  const F = mimc7.F;
+
+  const plaintext : Plaintext = ciphertext.data.map(
+    (e: bigint, i: number): bigint => {
+      return e - F.toObject(mimc7.hash(
+        sharedKey, 
+        ciphertext.iv + BigInt(i)
+      ));
+    }
+  );
+
+  return plaintext;
 };
 
 export {
